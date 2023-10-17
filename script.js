922caf0585c62336a8ec1712bad4e82fdecb220a
Loïc <e282266@edu.scbl.ch>
2023-10-17 11:24:09 +0200

    solved task 1-6 started task 7

--- script.js
+++ script.js
@@ -6,3 +6,35 @@ function changeBackground() {
   const body = document.querySelector("body")
   body.style.backgroundColor = "darkgrey"
 }
+
+function deleteText() {
+  const ToDeleteELem = document.querySelector("#to-delete")
+  ToDeleteELem.textContent = "Inhalt gelöscht"
+}
+
+function incrementCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num + 1
+  numberElement.textContent = num
+}
+
+function decrementCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num - 1
+  numberElement.textContent = num 
+}
+
+function resetCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = 0
+  numberElement.textContent = num 
+}
+
+function writeText() {
+  const textContentElem = document.querySelector("output")
+  const textInput = document.querySelector("#input")
+  textContentElem.textContent = textContentElem
+}
